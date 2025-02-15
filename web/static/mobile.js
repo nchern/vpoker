aea2e6e9655416455d7fda000737cfc5f1eed560
nchern <nchern@inbox.ru>
2025-02-15 20:53:18 +0100

    feat: add wakelock to the table page

---
+++ web/static/mobile.js
@@ -0,0 +1,29 @@
+let wakeLock;
+
+async function requestWakeLock() {
+    try {
+        if (!navigator.wakeLock) {
+            console.info('wakeLock API is not supported')
+            return;
+        }
+        wakeLock = await navigator.wakeLock.request('screen');
+        wakeLock.addEventListener('release', () => {
+            console.log('Wake lock released');
+        });
+        console.log('Wake lock is active');
+    } catch (err) {
+        console.error('Wake lock request failed:', err);
+    }
+}
+
+function initWakeLock() {
+    // Request wake lock when the page loads
+    document.addEventListener('visibilitychange', () => {
+        if (document.visibilityState === 'visible') {
+            requestWakeLock();
+        } else if (wakeLock) {
+            wakeLock.release();
+            wakeLock = null;
+        }
+    });
+}
